bb5edd18804996cb2b557e9eda2916ffa56a74ef
Dwi Siswanto <dwi.siswanto98@gmail.com>
2021-06-22 14:23:22 +0700

    Using into_inter instead

--- src/builder.rs
+++ src/builder.rs
@@ -3,14 +3,13 @@ mod payload;
 use url::Url;
 
 pub fn query(url: String) -> Vec<String> {
-	let mut urls: Vec<String> = vec![String::new(); 0];
 	let payload = payload::get();
 
-	for p in payload {
-		if let Ok(build) = Url::parse_with_params(&url, &[(p, "reserved")]) {
-			urls.push(build.as_str().to_string())
-		}
-	}
-
-	urls.into_iter().collect()
+	payload.into_iter()
+		.filter_map(|p| Url::parse_with_params(&url, &[(p, "reserved")])
+			.ok())
+		.map(|url| url
+			.as_str()
+			.to_string())
+		.collect()
 }
