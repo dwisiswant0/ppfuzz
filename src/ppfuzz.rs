b3a6f35041f77e4cb80d6b1554fd39fb53dbf8a8
Dwi Siswanto <dwi.siswanto98@gmail.com>
2021-06-24 03:02:51 +0700

    Seperate check function & handle can't find context with specified id

---
+++ src/ppfuzz.rs
@@ -0,0 +1,32 @@
+use {
+	chromiumoxide::Browser,
+	std::sync::Arc,
+	futures::StreamExt,
+	colored::*
+};
+
+pub async fn check(urls: Vec<String>, browser: Browser) {
+	let browser = Arc::new(browser);
+	let check = "(window.ppfuzz || Object.prototype.ppfuzz) == 'reserved' && true || false";
+	let mut stream = futures::stream::iter(urls.into_iter().map(|url| (url, Arc::clone(&browser))).map(|(url, browser)| async move {
+		let page = browser.new_page(&url).await.unwrap();
+		let vuln: bool = match page.evaluate(check).await {
+				Ok(res) => res.into_value().unwrap(),
+				Err(_err) => false,
+			};
+
+		Ok::<_, Box<dyn std::error::Error>>((url, vuln, page))
+	})).buffer_unordered(25);
+
+	while let Some(res) = stream.next().await {
+		if let Ok((ref url, vuln, page)) = res {
+			if vuln {
+				println!("[{}] {}", "VULN".green(), url)
+			} else {
+				eprintln!("[{}] {}", "ERRO".red(), url)
+			}
+
+			page.close().await.unwrap();
+		}
+	}
+}
